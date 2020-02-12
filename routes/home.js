5e6a84643d65c0d1d1557ee7137b4e3a5181768e
snipspin <photonenimpuls@gmail.com>
2020-02-11 23:15:17 -0800

    supports C/F zip and city

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+router.use(bodyParser.raw());
+
 router.get('/', (req,res) => {
-    res.render('home', {data: [1,2,3,4,5]});
+    res.render('home');
 });
 
 router.post('/', (req,res)=>{
-    res.render('present');
+    let zip = req.body.zip.length > 0 ? req.body.zip : req.body.place;
+    let unit = req.body.unit.length > 0 ? req.body.unit : 'C';
+    res.redirect(`/weather/${zip}/${unit}`);
 });
 
 module.exports = router;
--- routes/weather.js
+++ routes/weather.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const weather = require('weather-js');
+
 const router = express.Router();
+let wdata;
+let unit = 'C';
+let query = '98101';
 
 router.get('/', (req,res) => {
     res.render('present');
 });
 
-router.post('/:zipcode', (req,res)=>{
-    console.log(req.params);
-    res.render('present');
+router.get('/:zipcode', (req,res)=>{
+    if (req.params.zipcode.length > 0) query = req.params.zipcode;
+    weather.find({search: `${query}`, degreeType: unit}, function(err, result) {
+        if(err) console.log(err);
+        wdata = result[0];
+        res.render('present', {data:wdata});
+      });
+
+    
+});
+
+router.get('/:zipcode/:unit', (req,res)=>{
+    if (req.params.zipcode.length > 0) query = req.params.zipcode;
+    if (req.params.unit.toUpperCase() == 'F') unit = 'F';
+    weather.find({search: `${query}`, degreeType: unit}, function(err, result) {
+        if(err) console.log(err);
+        wdata = result[0];
+        res.render('present', {data:wdata});
+    });
 });
 
 module.exports = router;
