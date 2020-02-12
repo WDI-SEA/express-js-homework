f9f08da2c27451166b6fb44ed6f64c167efa2948
Melissa Young <melissay94@yahoo.com>
2020-02-11 23:28:24 -0800

    Added results to result page and styled

--- app.js
+++ app.js
@@ -39,8 +39,11 @@ app.post("/", (req, res) => {
 app.get("/weather/:zipcode", (req, res) => {
     getWeather(req.params.zipcode)
         .then((results) => {
-            console.log("Hi", results);
-            res.render("results.ejs", { weather: results });
+            let location = JSON.parse(results)[0].location;
+            let current = JSON.parse(results)[0].current;
+            let forecast = JSON.parse(results)[0].forecast;
+
+            res.render("results.ejs", { location: location, current: current, forecast: forecast });
         })
         .catch((err) => {
             res.send(404);
