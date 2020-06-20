755c1f80edbe6a4ca1b1247b8a2bf91f2f1cac08
Eliott R Davidson <fussypuppet@Eliotts-MacBook-Pro.local>
2020-06-20 13:18:05 -0700

    Big refactor job.  Random weather button now uses a tiny bit of embedded JS in its onclick attribute instead of importing an entire separate JS file from a static folder.  Also collapsed repetitive server code into a reusable function

--- app.js
+++ app.js
@@ -2,46 +2,38 @@ const express = require('express');
 const weather = require('weather-js');
 const weatherApp = express();
 weatherApp.set('viewengine', 'ejs');
-weatherApp.use(express.static('static'));
+
+
+function retrieveWeather (zipCode, res){
+    weather.find({search: zipCode, degreeType: 'F'}, function(err, result){
+        if (err) {
+            console.log(err);
+            res.render('weather.ejs', {message: `Error: ${err}`});
+        } else {
+            console.log("result", result);
+            if (result[0]){
+                console.log("forecast", result[0].forecast);
+                let responseMessage = `Weather for ${result[0].location.name} ${zipCode}: ${result[0].current.skytext}, ${result[0].current.temperature}°F`;
+                res.render('weather.ejs', {message: responseMessage});
+            } else {
+                res.render('weather.ejs', {message: `Could not retrieve weather for zip code ${zipCode}.`});
+            }
+        }
+    })
+}
+
 
 weatherApp.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {randomZip: Math.floor(Math.random()*100000)});
 })
-//weatherApp.get('/weather/:zipcode', (req, res) => {
 weatherApp.get('/weather', (req,res) => {
     //let thisZip = req.params.zipcode;
-    weather.find({search: req.query.zip_code, degreeType: 'F'}, function(err, result){
-        if (err) {
-            console.log(err);
-            res.render('weather.ejs', {message: `Error: ${err}`});
-        } else {
-            console.log("result", result);
-            if (result[0]){
-                let responseMessage = `Weather for ${result[0].location.name}: ${result[0].current.skytext}, ${result[0].current.temperature}°F`;
-                res.render('weather.ejs', {message: responseMessage});
-            } else {
-                res.render('weather.ejs', {message: 'Could not retrieve weather for that zip code'});
-            }
-        }
-    })
+    retrieveWeather(req.query.zip_code, res);
+})
+weatherApp.get('/weather/:randomZip', (req,res) => {
+    retrieveWeather(req.params.randomZip, res);
 })
 
-weatherApp.get('/weather/:randomZip', (req,res) => {
-    weather.find({search: req.params.randomZip, degreeType: 'F'}, function(err, result){
-        if (err) {
-            console.log(err);
-            res.render('weather.ejs', {message: `Error: ${err}`});
-        } else {
-            console.log("result", result);
-            if (result[0]){
-                let responseMessage = `Weather for ${result[0].location.name}: ${result[0].current.skytext}, ${result[0].current.temperature}°F`;
-                res.render('weather.ejs', {message: responseMessage});
-            } else {
-                res.render('weather.ejs', {message: 'Could not retrieve weather for that zip code'});
-            }
-        }
-    })
-})
 
 weatherApp.listen(8000, () => {
     console.log("Listening at 8000");
