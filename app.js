e6cb0f244f8b79aec21adae85aa9b98e67c058e9
Eryoung-610 <eryoung610@gmail.com>
2020-06-19 23:07:03 -0700

    Base functionlity working, attempting bonuses

---
+++ app.js
@@ -0,0 +1,32 @@
+let express = require('express');
+let app = express();
+let weather = require('weather-js');
+let ejsLayouts = require('express-ejs-layouts');
+
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+})
+
+app.get("/weather", (req, res) => {
+    weather.find({search: req.query.zipcode ,degreeType: "F"}, (err, result) => {
+        if(err) {
+            res.render("weather.ejs", {message : `ERROR: ${err}`});
+        } else {
+            // console.log(result[0])
+            if(result[0]){
+                let output = `Weather for ${result[0].location.name}: ${result[0].current.skytext}, ${result[0].current.temperature}°F`;
+
+                res.render("weather.ejs",{result: result[0], message: output});
+            } else {
+                res.render("weather.ejs", {message: "Invalid Zip code"});
+            }
+        }
+    })
+})
+
+
+app.listen(8000);
