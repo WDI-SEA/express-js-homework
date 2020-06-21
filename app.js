fa6b852eee73317e87e0837a1c483e9858d67cb4
Nick Hvattum <nhvattum@gmail.com>
2020-06-20 22:32:22 -0700

    I keep forgetting to commit

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-let express = require('express')
-let app = express()
-let weather = require('weather-js')
+let express = require('express');
+let app = express();
+let weather = require('weather-js');
+let ejslayouts = require('express-ejs-layouts');
 
-
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+// app.use(ejslayouts);
 
 app.get('/', (req, res) => {
     res.render('index')
-})
+});
 
-app.get('/weather', (req, res) => {
-    weather.find({search: req.query.zipcode, degreeType: 'F'}, function(err, result) {
+app.get('/weather/', (req, res) => {
+    weather.find({search: req.query.zipcode, degreeType: 'F', timezone: '-7'}, function(err, result) {
         if(err) {
             console.log(err);
         }
-    
-    res.render('weather')})
-    
-})
+
+        // I got stuck and had to reference Sarah's solution.  I don't fully understand what this is doing
+        res.render('results', { query: req.query.zipcode, result: result[0]})
+    })
+});
 
 
-app.listen(8000, () => {console.log("I hope it works")})
+app.listen(8000, () => {console.log("I hope it works")});
