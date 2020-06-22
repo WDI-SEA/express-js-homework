bf9bb312f8a65fee912452a7557e6500bdaf7c0a
Caitlin Smith <caitlinelizabeth888@gmail.com>
2020-06-21 18:09:43 -0700

    retrieved info from value field

--- app.js
+++ app.js
@@ -17,3 +17,26 @@ app.get('/weatherResults', (req, res) => {
 
 let port = 8000
 app.listen(port, () => {console.log(`I figured it out!`)})
+
+let weather = require('weather-js');
+
+// weather.find({search: '', degreeType: 'F'}, function(err, result) {
+//   if(err) console.log(err);
+  
+//   console.log(JSON.stringify(result, null, 2));
+// });
+
+
+app.get('/zipcode/', (req, res) => {
+    console.log(req.query);
+  })
+
+// weather.find({search: zipcode, degreeType: 'F'}, function(err, result) {
+//   if(err) {
+//   console.log(err) 
+//   } else {
+
+//   }
+// }})
+
+
