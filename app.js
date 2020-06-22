74540826fe5baf5cb288c9655b1499b5b01d9a15
Paul Padian <paulpadian@Pauls-MacBook-Pro.local>
2020-06-22 08:33:40 -0600

    working

--- app.js
+++ app.js
@@ -9,18 +9,19 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-app.get('/weather', (req, res) =>  {
-    let code = JSON.stringify(req.query.zip) 
+app.get('/weather', (req, res) => {
+    let code = JSON.stringify(req.query.zip)
     let currentWeather
-    weather.find({search: code, degreeType: 'F'}, function(err, result) {
-        if(err) console.log(err);
-        console.log(JSON.stringify(result, null, 2));
-        console.log(JSON.stringify(result[0].location.name));
-        currentWeather = [result[0].location.name,  result[0].current.temperature, result[0].current.winddisplay, result[0].current.day]
-        res.render('weather.ejs', {weathers: [currentWeather[3], currentWeather[0], `${currentWeather[1]} degrees Fahrenheit`, `With winds of ${currentWeather[2]}`]})
-    }); 
-   
-   
+    weather.find({
+        search: code,
+        degreeType: 'F'
+    }, function (err, result) {
+        if (err) console.log(err);
+        currentWeather = [result[0].location.name, result[0].current.temperature, result[0].current.winddisplay, result[0].current.day]
+        res.render('weather.ejs', {
+            weathers: [currentWeather[0], `${currentWeather[1]} degrees Fahrenheit`, currentWeather[3], `Winds of ${currentWeather[2]}`]
+        })
+    });
 })
 
 
