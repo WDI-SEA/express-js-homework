0dc62d29ca7a9cf3387b0df8ca947d9686805a19
Salman Malik <salmanmalik@Salmans-MacBook-Air.local>
2020-06-21 19:11:05 -0500

    Stubbed out routes and home and results ejs pages with forms

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Yoo this app is gonna be litt!')
+let express = require('express');
+let weather = require('weather-js');
+// let ejsLayouts = require('express-ejs-layouts');
+let app = express();
+
+//For linking CSS to our viewing engine
+app.use(express.static(__dirname + "/static"));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.get("/weather/:zipcode", (req, res) => {
+    res.send(`Results for ${req.params.zipcode}`)
+})
+
+
+
+
+app.listen(8000, () => {console.log('Singing and dancing on port 8000!!')})
