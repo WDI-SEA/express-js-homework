8eb53303db7384abb32750a3cc808036301f112a
Louis <lreillywillhight@gmail.com>
2020-06-21 17:34:25 -0700

    minor refactor, bugfixes

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ let fs = require('fs')
 let weather = require('weather-js')
 // returns a stringified JSON object
 
-let weatherLog = weather.find({ search: '98101', degreeType: 'F' }, function (err, result) {
-  if (err) { console.log(err) }
-  console.log(JSON.stringify(result, null, 2))
-})
+// let weatherLog = weather.find({ search: '98101', degreeType: 'F' }, function (err, result) {
+//   if (err) { console.log(err) }
+//   console.log(JSON.stringify(result, null, 2))
+// })
 
 // let currentWeather = weather.find({ search: '98101', degreeType: 'F' }, function (err, result) {
 //   if (err) { console.log(err) }
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 app.get('/weather', (req, res) => {
   weather.find({ search: req.query.zipCode, degreeType: 'F' }, function (err, result) {
     if (err) { console.log(err) }
-    res.render('weather.ejs', ({currentWeather:result}))
+    res.render('weather.ejs', ({currentWeather:result[0]}))
   })
   // let weatherData = JSON.parse(weather);
   // console.log(weather.find({ search: '98101', degreeType: 'F' }))
