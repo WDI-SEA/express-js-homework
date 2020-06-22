1ae0b80dec4fdb5f6b38e53eb5cbefe9398c00d6
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-06-22 09:53:41 -0700

    express homework

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 let express = require('express');
+let weather = require('weather-js');
 let app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
-    res.send("hey");
+  res.render('home');
 })
 
-app.get('/weather/:zipcode', (req, res) => {
-    res.send(`Resuslts ${req.params.zipcode}`);
+app.get('/weather', (req, res) => {
+    console.log(req.query)
+    weather.find({search: req.query.zipcode, degreeType: 'F'}, function(err, result) {
+        if(err)  {
+            console.log(err);
+        }    
+        res.render('results', { weather: req.query.zipcode, result: result[0] })
+    })
 })
 
-app.listen(5000, () => {
-    console.log("hey")
-});
+app.listen(8000, () => {
+    console.log("HELLO")
+})
