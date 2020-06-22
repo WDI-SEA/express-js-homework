bc8053fb6cd0dc8fd54fc723fb6cf74ecf23f74b
Noa Kim <noanonoa@gmail.com>
2020-06-21 23:51:25 -0700

    simplistic styling & bonus feature

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 let express = require('express');
-let app = express();
 let weather = require('weather-js');
 
+let app = express();
+
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 //homepage: index.ejs
 app.get('/', (req, res) => {
     res.render('index');
 });
+//form action page (GET method attached to req.query)
 app.get('/weather', (req, res) => {
-    res.send('YOU LINKED THE WEATHER RESULTS! Params: ' + req.query.weather)
-});
+    weather.find(
+        {
+            search: req.query.zip,
+            degreeType: 'F'
+        },
+        function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result)
 
+                res.render(
+                    'weather',
+                    {
+                        locationName: result[0].location.name,
+                        locationZip: result[0].location.zipcode,
+                        date: result[0].current.date,
+                        day: result[0].current.day,
+                        currentImg: result[0].current.imageUrl,
+                        skyText: result[0].current.skytext,
+                        temperature: result[0].current.temperature,
+                        humidity: result[0].current.humidity,
+                        temp: parseInt(result[0].current.temperature)
+                    }
+                )
+            }
+        }
+    )
+});
 
 app.listen(8000, () => {
     console.log(`let's build this thing 🚀`);
