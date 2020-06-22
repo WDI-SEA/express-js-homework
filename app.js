1315c5af1e05615135b606294c3e022612e8f74b
Louis <lreillywillhight@gmail.com>
2020-06-22 12:24:12 -0700

    added a random zipcode GET button

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ let ejsLayouts = require('express-ejs-layouts')
 
 app.use(express.static(__dirname + "/static"));
 
-let fs = require('fs')
-
 let weather = require('weather-js')
-// returns a stringified JSON object
+
+let zipcodes = require('zipcodes')
+var zipObject = zipcodes.random()
 
 // let weatherLog = weather.find({ search: '98101', degreeType: 'F' }, function (err, result) {
 //   if (err) { console.log(err) }
@@ -35,12 +35,17 @@ app.get('/weather', (req, res) => {
     if (err) { console.log(err) }
     res.render('weather.ejs', ({currentWeather:result[0]}))
   })
-  // let weatherData = JSON.parse(weather);
-  // console.log(weather.find({ search: '98101', degreeType: 'F' }))
+})
 
-
-  // res.render('weather.ejs', currentWeather)
-  // weatherJSON
+app.get('/weatherRandom', (req, res) => {
+  // console.log(zipObject.zip)
+  weather.find({ search: zipObject.zip, degreeType: 'F' }
+  , function (err, result) {
+    if (err) { console.log(err) }
+    // console.log({currentWeather:result})
+    
+    res.render('weatherRandom.ejs', ({currentWeather:result}))
+  })
 })
 
 // weather.find({search: '98101', degreeType: 'F'}, function(err, result) {
