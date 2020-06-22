ac8307165544213ccb2da6a1be2e06dc910b5447
zbarovsky <zachary.barovsky@gmail.com>
2020-06-21 17:20:17 -0700

    attempting to commit after last error

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/weather', (req,res) => {
-    
+    //res.render('weather.ejs', {req: req.query, res: res.query});
     for (let key in req.query) {
         weather.find({search: req.query[key], degreeType: 'F'}, function(err, result) {
 
@@ -28,10 +30,11 @@ app.get('/weather', (req,res) => {
                     } else if (key === 'skytext') {
                         res.write(value + " outside" + "\n");
                     }
-                }); res.send();
-            }
-        })
-    }
+                })  
+            } res.send();
+        }); 
+        
+    } 
 })
 
 app.listen(8000, () => {console.log("Dancing with myself on port 8000")});
