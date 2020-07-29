01955ccd3de15882a14ede7d5b67749b7bd791b5
jimmygranadino <jimmy.granadino@gmail.com>
2020-07-28 23:52:25 -0700

    level 5 complete

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 let express = require('express')
 let weather = require('weather-js')
+const bodyParser = require('body-parser')
 
 let app = express()
 
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.static('public'))
 
-app.get('/weather', (req, res) => {
-    weather.find({search: 'San Francisco, CA', degreeType: 'F'}, (err, result) => {
-        res.render('weather')
-    })
-  
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
-app.get('/currenttemp', (req, res) => {
-    weather.find({search: 'San Francisco, CA', degreeType: 'F'}, (err, result) => {
-        res.render('currenttemp', {query: 'San Francisco, CA', result: result[0]})
+app.get('/weather', (req, res) => {
+    weather.find({search: req.query.city, degreeType: 'F'}, (err, result) => {
+        res.render('weather', {query: req.query.city, result: result[0]})
     })
   
 })
 
 app.listen(8000, function() {
-    console.log("🧐")
+    console.log("🌊🌊🌊")
 })
